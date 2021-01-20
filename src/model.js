4c0a8a228ee33b0abad899446cffd201f703ec6a
Kazi Jawad <kazi.a.jawad@gmail.com>
2021-01-20 11:39:52 -0500

    connect audio input to model

--- src/model.js
+++ src/model.js
@@ -1,4 +1,26 @@
-import Pitchfinder from 'pitchfinder';
+import Pitchfinder from "pitchfinder";
+
+async function playMusic(noteSequence) {
+    try {
+        const mm = await import("@magenta/music/es6");
+
+        const player = new mm.Player();
+
+        const model = new mm.MusicRNN("https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/melody_rnn");
+        model.initialize();
+
+        if (player.isPlaying()) {
+            player.stop();
+            return;
+        }
+    
+        const quantizedNoteSequence = mm.sequences.quantizeNoteSequence(noteSequence, 4);
+        const sample = await model.continueSequence(quantizedNoteSequence, 20, 0.5);
+        player.start(sample);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 async function handleAudio() {
     try {
@@ -37,7 +59,7 @@ async function handleAudio() {
             if (currentTime >= 5000) {
                 clearInterval(recordAudio);
                 microphone.close();
-                console.log(noteSequence);
+                playMusic(noteSequence);
             }
         }, 100);
     } catch (error) {
