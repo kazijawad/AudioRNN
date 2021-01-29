d63187dfa78ef683c626deb4219f9d9ac583e3ba
Kazi Jawad <kazi.a.jawad@gmail.com>
2021-01-28 21:08:56 -0500

    add base timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -2,4 +2,12 @@ class Timeline {
     constructor(containerID) {
         this.container = document.getElementById(containerID);
     }
+
+    render() {
+        const playButton = document.createElement("button");
+        playButton.textContent = "Play";
+        this.container.appendChild(playButton);
+    }
 }
+
+export default Timeline;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./styles/index.css";
 import StageC from './components/stage';
+import TimelineC from './components/timeline';
 
 const Stage = new StageC("stage");
 Stage.render();
+
+const Timeline = new TimelineC("timeline");
+Timeline.render();
